b993e319c080edb56f7a32cf37ed6418bc382f71
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2025-03-11 12:30:51 +0100

    Añadir API Gateway para invocacion al API REST Conversion

--- src/main/java/com/in28minutes/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/in28minutes/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.in28minutes.microservices.currency_conversion_service.entity.CurrencyExchange;
 
-@FeignClient(name="currency-exchange",url="http://localhost:8000",path="/currency-exchange")
+// FeignClient lleva integrado de forma automatica el Spring Load Balancer
+//@FeignClient(name="currency-exchange",url="http://localhost:8000",path="/currency-exchange")
+@FeignClient(name="currency-exchange",path="/currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/from/{from}/to/{to}")
